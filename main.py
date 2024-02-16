6b39f423b8168f0d96970e510c78129fa2f0a968
jayce <louis821110@gmail.com>
2024-02-16 16:28:29 +0800

    edit readme

--- main.py
+++ main.py
@@ -50,9 +50,17 @@ def callback():
 def handle_message(event):
     input_text = event.message.text
     if '?' in input_text:
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='請輸入 起點,終點,時間 (使用,分開)站名可使用代號或部分中文站名表示\ne.g.\n\nA8,高鐵,!1400\n表示A8長庚醫院站到A18桃園高鐵站且"出發時間"為14點整\n\n北車,長庚,@1400\n表示A1台北車站到A8長庚醫院站且"抵達時間"為14點整\n\n北車,二航廈\n表示A1台北車站到A13機場第二航廈站且"出發時間"為現在\n\n\n現在有支援直達車查詢囉!可以試試看\n\n#1,8,@1400\n\n表示A1台北車站到A8長庚醫院站且"抵達時間"為14點整的直達車！'))
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='請輸入 起點,終點,時間 (使用,分開)站名可使用代號或部分中文站名表示\
+                                                                     \ne.g.\n\nA8,高鐵,!1400\n表示A8長庚醫院站到A18桃園高鐵站且"出發時間"為14點整\
+                                                                     \n\n北車,長庚,@1400\n表示A1台北車站到A8長庚醫院站且"抵達時間"為14點整\
+                                                                     \n\n北車,二航廈\n表示A1台北車站到A13機場第二航廈站且"出發時間"為現在\
+                                                                     \n\n\n現在有支援直達車查詢囉!可以試試看\n\n#1,8,@1400\n\n表示A1台北車站到A8長庚醫院站且"抵達時間"為14點整的直達車！'))
     elif "？" in input_text:
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='請輸入 起點,終點,時間 (使用,分開)站名可使用代號或部分中文站名表示\ne.g.\n\nA8,高鐵,!1400\n表示A8長庚醫院站到A18桃園高鐵站且"出發時間"為14點整\n\n北車,長庚,@1400\n表示A1台北車站到A8長庚醫院站且"抵達時間"為14點整\n\n北車,二航廈\n表示A1台北車站到A13機場第二航廈站且"出發時間"為現在\n\n\n現在有支援直達車查詢囉!可以試試看\n\n#1,8,@1400\n\n表示A1台北車站到A8長庚醫院站且"抵達時間"為14點整的直達車！'))
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='請輸入 起點,終點,時間 (使用,分開)站名可使用代號或部分中文站名表示\
+                                                                     \ne.g.\n\nA8,高鐵,!1400\n表示A8長庚醫院站到A18桃園高鐵站且"出發時間"為14點整\
+                                                                     \n\n北車,長庚,@1400\n表示A1台北車站到A8長庚醫院站且"抵達時間"為14點整\
+                                                                     \n\n北車,二航廈\n表示A1台北車站到A13機場第二航廈站且"出發時間"為現在\
+                                                                     \n\n\n現在有支援直達車查詢囉!可以試試看\n\n#1,8,@1400\n\n表示A1台北車站到A8長庚醫院站且"抵達時間"為14點整的直達車！'))
     else:
         line_bot_api.reply_message(event.reply_token,TextSendMessage(text=get_all_train.main(input_text)))
         
@@ -62,4 +70,4 @@ def handle_message(event):
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port )
